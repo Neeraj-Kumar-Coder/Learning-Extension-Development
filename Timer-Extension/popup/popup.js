8273234edd876d9fd07dcf24d5365254ee815abe
Neeraj Kumar <neeraj4.intern@phonepe.com>
2024-06-25 11:50:57 +0530

    fix: made the button group visible only when the clock and timer are loaded, also increased the size of the popup

--- Timer-Extension/popup/popup.js
+++ Timer-Extension/popup/popup.js
@@ -1,5 +1,6 @@
 const timeElement = document.getElementById("currentTime");
 const timerElement = document.getElementById("timer");
+const btnHolder = document.querySelector(".btn-holder");
 const startBtn = document.getElementById("startBtn");
 const stopBtn = document.getElementById("stopBtn");
 const resetBtn = document.getElementById("resetBtn");
@@ -14,6 +15,7 @@ chrome.storage.sync.get(["name"], (syncResult) => {
             const currentTime = new Date().toLocaleTimeString();
             timeElement.textContent = `Hello ${name}, the current time is: ${currentTime}`;
             timerElement.textContent = `The timer is at: ${timer} seconds`;
+            btnHolder.style.display = "flex";
         });
     }, 1000);
 });
