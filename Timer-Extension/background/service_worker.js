2083100f66e8e1bba797753f0cccf03fd3747283
Neeraj Kumar <neeraj4.intern@phonepe.com>
2024-06-25 11:29:15 +0530

    feat: Added start, stop and reset option in the timer

--- Timer-Extension/background/service_worker.js
+++ Timer-Extension/background/service_worker.js
@@ -1,19 +1,22 @@
 chrome.alarms.create("timer-alarm", {
-    periodInMinutes: 1 / 60
+    periodInMinutes: 1 / 60,
 });
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-    chrome.storage.local.get(["timer"], (result) => {
+    chrome.storage.local.get(["timer", "isRunning"], (localResult) => {
+        const isRunning = localResult.isRunning ?? true;
+        if (!isRunning) return;
+
         chrome.storage.sync.get(["interval"], (syncResult) => {
             const interval = syncResult.interval ?? -1;
-            const time = result.timer ?? 0;
-            chrome.storage.local.set({ timer: time + 1 });
+            const timer = localResult.timer ?? 0;
+            chrome.storage.local.set({ timer: timer + 1 });
 
-            if (interval !== -1 && (time + 1) % interval === 0)
+            if (interval !== -1 && (timer + 1) % interval === 0)
                 this.registration.showNotification("Chrome timer extension", {
                     body: `${interval} seconds have passed!`,
-                    icon: "../icons/icon-64.png"
-                })
-        })
+                    icon: "../icons/icon-64.png",
+                });
+        });
     });
 });
--- Timer-Extension/popup/popup.js
+++ Timer-Extension/popup/popup.js
@@ -1,5 +1,8 @@
 const timeElement = document.getElementById("currentTime");
 const timerElement = document.getElementById("timer");
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const resetBtn = document.getElementById("resetBtn");
 
 chrome.storage.sync.get(["name"], (syncResult) => {
     const name = syncResult.name ?? "Stranger";
@@ -11,9 +14,28 @@ chrome.storage.sync.get(["name"], (syncResult) => {
             const currentTime = new Date().toLocaleTimeString();
             timeElement.textContent = `Hello ${name}, the current time is: ${currentTime}`;
             timerElement.textContent = `The timer is at: ${timer} seconds`;
-        })
+        });
     }, 1000);
-})
+});
+
+startBtn.addEventListener("click", () => {
+    chrome.storage.local.set({
+        isRunning: true,
+    });
+});
+
+stopBtn.addEventListener("click", () => {
+    chrome.storage.local.set({
+        isRunning: false,
+    });
+});
+
+resetBtn.addEventListener("click", () => {
+    chrome.storage.local.set({
+        timer: 0,
+        isRunning: false,
+    });
+});
 
 // chrome.action.setBadgeText({
 //     text: "TIME"
